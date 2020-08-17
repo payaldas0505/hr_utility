94fcb69c178b1d9c7425a1c5224dfd640cc372c3
Your Name <payaldas0505>
2020-08-17 19:42:17 +0530

    solved logout merged issue

--- static/js/logout.js
+++ static/js/logout.js
@@ -1,26 +1,26 @@
 // log out
 
 function logout() {
-  var userDetails = getValues('UserDetails')
-  var access = userDetails.access
-  $.ajax({
-      type: 'GET',
-      url: '/logout/',
-      headers: { Authorization: 'Bearer '+ access},
-      success: function (data) {
-        // parsed_jsondata = JSON.parse(result)
-        // console.log('result',parsed_jsondata)
-        window.localStorage.clear();
-        window.sessionStorage.clear();
-        window.location.href = data.url;
+    var userDetails = getValues('UserDetails')
+    var access = userDetails.access
+    $.ajax({
+        type: 'GET',
+        url: '/logout/',
+        headers: { Authorization: 'Bearer ' + access },
+        success: function(data) {
+            // parsed_jsondata = JSON.parse(result)
+            // console.log('result',parsed_jsondata)
+            window.localStorage.clear();
+            window.sessionStorage.clear();
+            window.location.href = data.url;
 
-        // location.href = result['template_name'];
-      },
-        error: function(data){
-          if (data.status == 401) {
-            getaccessToken(logout);
+            // location.href = result['template_name'];
+        },
+        error: function(data) {
+            if (data.status == 401) {
+                getaccessToken(logout);
 
-         }
-      }
+            }
+        }
     })
-  }
+}
