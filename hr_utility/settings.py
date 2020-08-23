dcc4f5d466424ea2d2368ec5c9a202b6b0bc8ce0
payaldas0505 <payaldas0505@gmail.com>
2020-08-23 19:29:29 +0530

    allowed host

--- hr_utility/settings.py
+++ hr_utility/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '7r%jq&x5y3g!dd+h-*rf&ma9g!w6r227q+j@sbe==hj)cv0z(2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
